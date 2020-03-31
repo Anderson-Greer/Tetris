b0a04666d8168b5a51cfb990114a489d89c84dc9
Andy Greer <andersonagme@gmail.com>
2020-03-31 18:05:34 -0400

    Architecture and functionality
    Architecture was wiped and remade for continuity and simplicity.  The straight block now loops on the screen.

--- Main.py
+++ Main.py
@@ -13,10 +13,30 @@ def select_block():
     return b
 
 
+def print_gameboard():
+    for row in gameboard:
+        print(row)
+    print()
+    print()
+
+
+def update_gameboard():
+    # Resets the board
+    for x in range(0, len(gameboard)):
+        for y in range(0, len(gameboard[0])):
+            gameboard[x][y] = 0
+
+    # Adds each block to the board
+    for element in blocks:
+        coords = element.get_coords()
+        for xy in coords:
+            gameboard[xy[0]][xy[1]] = 1
+
+
 pygame.init()
 
 # Keeps track of blocks
-gameBoard = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+gameboard = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -38,6 +58,7 @@ gameBoard = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 game_over = False
+blocks = []
 
 # Dictionary of colors for the blocks
 colors = {
@@ -54,14 +75,28 @@ clock = pygame.time.Clock()
 
 while not game_over:
     block = select_block()
+    blocks.append(block)
 
     # Runs each "turn" for a block i.e. from when it spawns to when it stops
     while not block.done:
-        block.move()
+        update_gameboard()
+        print_gameboard()
+
+        block.move_down()
 
         for event in pygame.event.get():
 
-            if event.type == pygame.QUIT:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    print("right arrow pressed")
+                elif event.key == pygame.K_LEFT:
+                    print("left arrow pressed")
+                elif event.key == pygame.K_UP:
+                    print("up arrow pressed")
+                elif event.key == pygame.K_DOWN:
+                    print("down arrow pressed")
+
+            elif event.type == pygame.QUIT:
                 sys.exit()
 
         clock.tick(2)
--- Shapes.py
+++ Shapes.py
@@ -10,37 +10,43 @@ class Block:
         self.width = 250
         self.height = 500
         self.screen = pygame.display.set_mode((self.width, self.height))  # Creates the GUI for the game
+        self.done = False  # Tracks if the block is finished or not
         self.color = color  # Color of each block
         self.RECT_SIZE = 25  # Constant for the size of each "pixel"
-        self.coordinates = []  # A list of tuples corresponding to points on the game board
-        self.num_of_blocks = 0
+        self.coordinates = [[]]  # A list of ordered pairs corresponding to points on the game board
 
         self.screen.fill((0, 0, 0))  # Makes the screen black
+
+    def is_done(self):
+        for coord in self.coordinates:
+            if coord[0] == 19:
+                self.done = True
+            else:
+                self.done = False
 
     def move_down(self):
         self.screen.fill((0, 0, 0))  # Makes the screen black
 
-        for i in range(0, self.num_of_blocks):
+        for i in range(0, len(self.coordinates)):
             pygame.draw.rect(self.screen, self.color, (self.coordinates[i][1] * 25, self.coordinates[i][0] * 25,
                                                        self.RECT_SIZE, self.RECT_SIZE))
 
+        self.is_done()
         for coord in self.coordinates:
-            coord[1] += 1
+            if not self.done:
+                coord[0] += 1
+
+    def get_coords(self):
+        return self.coordinates
 
 
 class StraightBlock(Block):
-
     y_value = 0
     starting_x = 0
-    done = False
 
-    def move(self):
-        self.screen.fill((0, 0, 0))
-        for i in range(0, 4):
-            pygame.draw.rect(self.screen, self.color, ((75 + 25 * i), self.y_value, self.RECT_SIZE, self.RECT_SIZE))
-
-        self.y_value += 25
-        print(self.y_value)
+    def __init__(self, color):
+        super(StraightBlock, self).__init__(color)
+        self.coordinates = [[0, 3], [0, 4], [0, 5], [0, 6]]
 
 
 class LeftL:
